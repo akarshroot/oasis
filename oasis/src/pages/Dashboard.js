8dfe20a441172a8409e7c515f7becee190466e50
Rashmika <rashmikadatta1@gmail.com>
2022-05-07 02:34:24 +0530

    dash-icons

--- oasis/src/pages/Dashboard.js
+++ oasis/src/pages/Dashboard.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './Dashboard.css'
+import userimg from '../assets/user.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartColumn } from '@fortawesome/free-solid-svg-icons';
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import { faHouseMedicalCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { faHouseCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faMaskFace } from '@fortawesome/free-solid-svg-icons';
 
 function Dashboard() {
   return (
     <div className="dash-container">
 
       <div className="column left" style={{backgroundColor: "white"}}>
-        <div className="dash-tabs">Dashboard</div>
-        <div className="dash-tabs">Vaccination History</div>
-        <div className="dash-tabs">Vaccination Due</div>
-        <div className="dash-tabs">COVID-19 Resources</div>
+        <div className="dash-tabs"><FontAwesomeIcon icon={ faChartColumn } className="dash-icon"/>Dashboard</div>
+        <div className="dash-tabs"><FontAwesomeIcon icon={ faFolderOpen } className="dash-icon"/>Health Status</div>
+        <div className="dash-tabs"><FontAwesomeIcon icon={ faHouseMedicalCircleCheck } className="dash-icon"/>Vaccination History</div>
+        <div className="dash-tabs"><FontAwesomeIcon icon={ faHouseCircleExclamation } className="dash-icon"/>Vaccination Due</div>
+        <div className="dash-tabs"><FontAwesomeIcon icon={ faMaskFace } className="dash-icon"/>COVID-19 Resources</div>
       </div>
 
       <div className="column middle" style={{backgroundColor:"#bbb"}}>
@@ -19,7 +27,7 @@ function Dashboard() {
 
       <div className="column right" style={{backgroundColor:"#ccc"}}>
         <div>
-          <img></img>
+        <img src={userimg} alt="User image" className="user-img"></img> 
         </div>
         <div>
 
