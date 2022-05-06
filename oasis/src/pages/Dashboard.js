c14cbf66183fc958734fb61288702980da961494
Rashmika <rashmikadatta1@gmail.com>
2022-05-06 22:06:29 +0530

    dash-temp

--- oasis/src/pages/Dashboard.js
+++ oasis/src/pages/Dashboard.js
@@ -5,18 +5,22 @@ function Dashboard() {
   return (
     <div className="dash-container">
 
-<div className="column left" style={{backgroundColor: "#aaa"}}>
-    <h2>Column 1</h2>
-    <p>Some text..</p>
-  </div>
-  <div className="column middle" style={{backgroundColor:"#bbb"}}>
-    <h2>Column 2</h2>
-    <p>Some text..</p>
-  </div>
-  <div className="column right" style={{backgroundColor:"#ccc"}}>
-    <h2>Column 3</h2>
-    <p>Some text..</p>
-  </div>
+      <div className="column left" style={{backgroundColor: "white"}}>
+        <div className="dash-tabs">Dashboard</div>
+        <div className="dash-tabs">Vaccination History</div>
+        <div className="dash-tabs">Vaccination Due</div>
+        <div className="dash-tabs">COVID-19 Resources</div>
+      </div>
+
+      <div className="column middle" style={{backgroundColor:"#bbb"}}>
+        <h2>Column 2</h2>
+        
+      </div>
+
+      <div className="column right" style={{backgroundColor:"#ccc"}}>
+        <h2>Column 3</h2>
+        <p>Some text..</p>
+      </div>
 
     </div>
   )
