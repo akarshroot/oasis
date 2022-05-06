4518cec0b54358b28460d95f570b6818d93dd1ac
Tanisha <tanishagupta0820@gmail.com>
2022-05-07 02:33:51 +0530

    Dashboard

--- oasis/src/pages/Dashboard.js
+++ oasis/src/pages/Dashboard.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import './Dashboard.css'
+import dashpic from '../assets/dashPic.png'
+import vacimg from '../assets/vacimg.png'
 
 function Dashboard() {
   return (
     <div className="dash-container">
 
       <div className="column left" style={{backgroundColor: "white"}}>
-        <div className="dash-tabs">Dashboard</div>
-        <div className="dash-tabs">Vaccination History</div>
+        <div className="dash-tabs"><img src = {dashpic} className = 'dash-pic dashbpic'></img>Dashboard</div>
+        <div className="dash-tabs"><img src = {vacimg} className = 'dash-pic vacimg'></img>Vaccination History</div>
         <div className="dash-tabs">Vaccination Due</div>
         <div className="dash-tabs">COVID-19 Resources</div>
       </div>
 
-      <div className="column middle" style={{backgroundColor:"#bbb"}}>
-        <h2>Column 2</h2>
+      <div className="column middle">
+        <div className = 'dash-wel'>
+         <b> Welcome to your dashboard!</b><br/>
+         <span className = 'check'>Check out what's new since your last visit.</span> 
+        </div>
         
       </div>
 
