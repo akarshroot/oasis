487d0e96b8f357c65680e9fd251d61849842cb5b
Rashmika <rashmikadatta1@gmail.com>
2022-05-06 19:55:02 +0530

    dash-division

--- oasis/src/pages/Dashboard.js
+++ oasis/src/pages/Dashboard.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Dashboard.css'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className="dash-container">
+
+<div className="column left" style={{backgroundColor: "#aaa"}}>
+    <h2>Column 1</h2>
+    <p>Some text..</p>
+  </div>
+  <div className="column middle" style={{backgroundColor:"#bbb"}}>
+    <h2>Column 2</h2>
+    <p>Some text..</p>
+  </div>
+  <div className="column right" style={{backgroundColor:"#ccc"}}>
+    <h2>Column 3</h2>
+    <p>Some text..</p>
+  </div>
+
+    </div>
   )
 }
 
